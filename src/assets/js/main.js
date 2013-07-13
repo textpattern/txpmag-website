b98851593eb83f0449ca8268037b2e814d3733de
Jukka Svahn <web@rahinaa.biz>
2013-07-14 00:15:56 +0300

    Old jQuery cookie doesn't return undefined.
    The $.show() is unnecessary.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -99,11 +99,10 @@
 
     require(['jquery', 'cookie'], function ($)
     {
-        if ($.cookie('acceptedCookies') === undefined)
+        if (!$.cookie('acceptedCookies'))
         {
             var disclaimer = $('<aside id="cookie-disclaimer"><div class="container"><p><strong>This website uses cookies to enhance your experience.</strong> By continuing to use this website you agree to cookies being placed on your computer. If you wish to use this website but do not wish for cookies to be placed on your computer you can change the settings in your internet browser. <a href="#" data-action="close">Close</a>.</p></div></aside>');
             $('body').prepend(disclaimer);
-            disclaimer.show();
             $.cookie('acceptedCookies', 1, {expires: 1461});
 
             disclaimer.find('a').click(function (e)
