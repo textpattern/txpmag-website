79813da35c31baaad1a2c03826f501cccd037760
Jukka Svahn <web@rahinaa.biz>
2013-07-31 13:58:30 +0300

    Load fonts from Google Fonts, using WebFont Loader.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -7,6 +7,7 @@
         {
             'jquery': '//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min',
             'jqueryui': '//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min',
+            'webfont' : '//ajax.googleapis.com/ajax/libs/webfont/1.4.6/webfont',
             'flowplayer': 'flowplayer/flowplayer.min'
         },
         shim:
@@ -143,6 +144,18 @@
                 });
             });
         }
+    });
+
+    // Fonts.
+
+    require(['webfont'], function ()
+    {
+        WebFont.load({
+            google:
+            {
+                families: ['PT+Serif:400,700,400italic,700italic:latin', 'Cousine::latin', 'Source+Sans+Pro:200,400,600,700,200italic,400italic,600italic,700italic:latin']
+            }
+        });
     });
 
     // Disqus.
