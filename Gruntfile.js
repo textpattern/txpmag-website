1e3996766c309a8eeee9e62994a54c631506774f
Phil Wareham <phil@designhive.com>
2013-12-21 18:01:23 +0000

    Build Modernizr via Grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,6 +7,7 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-jshint');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-modernizr');
     grunt.loadNpmTasks('grunt-shell');
 
     grunt.initConfig({
@@ -132,6 +133,36 @@ module.exports = function (grunt) {
             }
         },
 
+        modernizr: {
+            'devFile': 'bower_components/modernizr/modernizr.js',
+            'outputFile': 'public/assets/js/modernizr.js',
+            'tests': [
+                'boxshadow',
+                'flexbox',
+                'rgba',
+                'svg',
+                'touch'
+            ],
+            'extra': {
+                'shiv': true,
+                'printshiv': false,
+                'load': false,
+                'mq': false,
+                'cssclasses': true
+            },
+            'extensibility': {
+                'addtest': false,
+                'prefixed': false,
+                'teststyles': true,
+                'testprops': true,
+                'testallprops': true,
+                'hasevents': false,
+                'prefixes': true,
+                'domprefixes': true
+            },
+            'parseFiles': false
+        },
+
         shell: {
             setup: {
                 command: [
@@ -147,7 +178,7 @@ module.exports = function (grunt) {
     grunt.registerTask('test', ['jshint']);
     grunt.registerTask('sass', ['compass', 'cssmin', 'copy:css']);
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['jshint', 'sass', 'copy:img', 'copy:js', 'uglify']);
+    grunt.registerTask('build', ['jshint', 'sass', 'copy:img', 'copy:js', 'uglify', 'modernizr']);
     grunt.registerTask('travis', ['jshint', 'compass']);
     grunt.registerTask('setup', ['shell:setup']);
 };
