2c57bb7edc06d2d9573b78e6fd835af8c2599fe2
Phil Wareham <phil@designhive.com>
2013-12-27 18:12:32 +0000

    Mangle variables

--- Gruntfile.js
+++ Gruntfile.js
@@ -108,7 +108,6 @@ module.exports = function (grunt) {
         uglify: {
             dist: {
                 options: {
-                    mangle: false,
                     preserveComments: 'some'
                 },
 
